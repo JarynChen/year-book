42adc646e7dcf5690262446435e6010b3881d7eb
Jaryn <Jaryn@126.com>
2023-01-15 16:34:27 +0800

    feat: update search page

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
 defineOptions({
-  name: "Welcome"
+  name: "Search"
 });
+import { ref } from "vue";
+const input = ref("");
 </script>
 
 <template>
-  <h1>Index</h1>
+  <el-row justify="center">
+    <el-col :span="24">
+      <el-space direction="vertical" alignment="center" :size="10">
+        <el-input size="large" v-model="input" placeholder="input" clearable />
+        <el-button type="primary">Search</el-button>
+      </el-space></el-col
+    >
+  </el-row>
 </template>
